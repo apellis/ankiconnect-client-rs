e9ec82dc46e378e53cd22113e16bd058e574b5d7
Alex Ellis <aellis@mbpt-aellis.ad.quora.com>
2019-06-20 13:54:22 -0700

    Implement several more endpoints

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,7 @@ extern crate serde;
 extern crate serde_json;
 
 use default::default;
+use std::error::Error;
 use std::fmt::Debug;
 use serde::{Serialize, Deserialize};
 use serde::de::DeserializeOwned;
@@ -37,7 +38,7 @@ impl<'a> AnkiConnectClient<'a> {
     }
 
     fn call<'b, ResultT>(&self, action: &'b str, params: Option<serde_json::Value>)
-            -> Result<ResultT, Box<std::error::Error>>
+            -> Result<ResultT, Box<Error>>
             where ResultT: Debug + DeserializeOwned
     {
         let request = AnkiConnectRequest {
@@ -73,19 +74,19 @@ impl<'a> AnkiConnectClient<'a> {
     // ========== Miscellaneous =============================================
     // ======================================================================
 
-    pub fn version(&self) -> Result<i64, Box<std::error::Error>> {
+    pub fn version(&self) -> Result<i64, Box<Error>> {
         Ok(self.call::<i64>("version", None)?)
     }
 
-    pub fn upgrade(&self) -> Result<bool, Box<std::error::Error>> {
+    pub fn upgrade(&self) -> Result<bool, Box<Error>> {
         Ok(self.call::<bool>("upgrade", None)?)
     }
 
-    pub fn sync(&self) -> Result<(), Box<std::error::Error>> {
+    pub fn sync(&self) -> Result<(), Box<Error>> {
         Ok(self.call::<()>("sync", None)?)
     }
 
-    pub fn load_profile(&self, username: &str) -> Result<bool, Box<std::error::Error>> {
+    pub fn load_profile(&self, username: &str) -> Result<bool, Box<Error>> {
         let params = json!({ "name": username });
         Ok(self.call::<bool>("loadProfile", Some(params))?)
     }
@@ -97,38 +98,100 @@ impl<'a> AnkiConnectClient<'a> {
     // ========== Decks =====================================================
     // ======================================================================
 
-    pub fn deck_names(&self) -> Result<Vec<String>, Box<std::error::Error>> {
+    pub fn deck_names(&self) -> Result<Vec<String>, Box<Error>> {
         Ok(self.call::<Vec<String>>("deckNames", None)?)
     }
 
-    pub fn deck_names_and_ids(&self) -> Result<HashMap<String, i64>, Box<std::error::Error>> {
+    pub fn deck_names_and_ids(&self) -> Result<HashMap<String, i64>, Box<Error>> {
         Ok(self.call::<HashMap<String, i64>>("deckNamesAndIds", None)?)
     }
 
-    // TODO
-    //   -- getDecks
-    //   -- createDeck
-    //   -- changeDeck
-    //   -- deleteDecks
-    //   -- getDeckConfig
-    //   -- saveDeckConfig
-    //   -- setDeckConfigId
-    //   -- cloneDeckConfigId
-    //   -- removeDeckConfigId
+    pub fn get_decks(&self, cards: &Vec<i64>)
+            -> Result<HashMap<String, Vec<i64>>, Box<Error>>
+    {
+        let params = json!({ "cards": &cards });
+        Ok(self.call::<HashMap<String, Vec<i64>>>("getDecks", Some(params))?)
+    }
+
+    pub fn create_deck(&self, deck: &str) -> Result<i64, Box<Error>> {
+        let params = json!({ "deck": deck });
+        Ok(self.call::<i64>("createDeck", Some(params))?)
+    }
+
+    pub fn change_deck(&self, cards: &Vec<i64>, deck: &str) -> Result<(), Box<Error>> {
+        let params = json!({ "cards": &cards, "deck": deck });
+        Ok(self.call::<()>("changeDeck", Some(params))?)
+    }
+
+    pub fn delete_decks(&self, decks: &Vec<String>, cards_too: bool) -> Result<(), Box<Error>> {
+        let params = json!({ "decks": &decks, "cardsToo": cards_too });
+        Ok(self.call::<()>("deleteDecks", Some(params))?)
+    }
+
+    pub fn get_deck_config(&self, deck: &str) -> Result<serde_json::Value, Box<Error>> {
+        let params = json!({ "deck": deck });
+        Ok(self.call::<serde_json::Value>("getDeckConfig", Some(params))?)
+    }
+
+    pub fn save_deck_config(&self, config: &serde_json::Value) -> Result<bool, Box<Error>> {
+        let params = json!({ "config": config });
+        Ok(self.call::<bool>("saveDeckConfig", Some(params))?)
+    }
+
+    pub fn set_deck_config_id(&self, decks: &Vec<String>, config_id: i64)
+            -> Result<bool, Box<Error>>
+    {
+        let params = json!({ "decks": decks, "configId": config_id });
+        Ok(self.call::<bool>("setDeckConfigId", Some(params))?)
+    }
+
+    // TODO cloneDeckConfigId
+
+    pub fn remove_deck_config_id(&self, config_id: i64) -> Result<bool, Box<Error>> {
+        let params = json!({ "configId": config_id });
+        Ok(self.call::<bool>("removeDeckConfigId", Some(params))?)
+    }
 
     // ======================================================================
     // ========== Models ====================================================
     // ======================================================================
 
-    pub fn model_names(&self) -> Result<Vec<String>, Box<std::error::Error>> {
+    pub fn model_names(&self) -> Result<Vec<String>, Box<Error>> {
         Ok(self.call::<Vec<String>>("modelNames", None)?)
     }
 
-    // TODO
-    //   -- modelNamesAndIds
-    //   -- modelFieldNames
-    //   -- modelFieldsOnTemplates
-    //   -- createModel
+    pub fn model_names_and_ids(&self) -> Result<HashMap<String, i64>, Box<Error>> {
+        Ok(self.call::<HashMap<String, i64>>("modelNamesAndIds", None)?)
+    }
+
+    pub fn model_field_names(&self, model_name: &str) -> Result<Vec<String>, Box<Error>> {
+        let params = json!({ "modelName": model_name });
+        Ok(self.call::<Vec<String>>("modelFieldNames", Some(params))?)
+    }
+
+    pub fn model_fields_on_templates(&self, model_name: &str)
+            -> Result<HashMap<String, Vec<Vec<String>>>, Box<Error>>
+    {
+        let params = json!({ "modelName": model_name });
+        Ok(self.call::<HashMap<String, Vec<Vec<String>>>>("modelFieldsOnTemplates", Some(params))?)
+    }
+
+    pub fn create_model(
+            &self,
+            model_name: &str,
+            in_order_fields: &Vec<String>,
+            css: &str,
+            card_templates: HashMap<String, String>)
+            -> Result<serde_json::Value, Box<Error>>
+    {
+        let params = json!({
+            "modelName": model_name,
+            "inOrderFields": in_order_fields,
+            "css": css,
+            "cardTemplates": &card_templates
+        });
+        Ok(self.call::<serde_json::Value>("createModel", Some(params))?)
+    }
 
     // ======================================================================
     // ========== Notes =====================================================
@@ -154,7 +217,7 @@ impl<'a> AnkiConnectClient<'a> {
     // invalide card ids
     // Warning 2: Return type is Option<bool> rather than bool for symmetry with unsuspend;
     // see warning 2 on that method.
-    pub fn suspend(&self, cards: &Vec<i64>) -> Result<Option<bool>, Box<std::error::Error>> {
+    pub fn suspend(&self, cards: &Vec<i64>) -> Result<Option<bool>, Box<Error>> {
         let params = json!({ "cards": &cards });
         Ok(self.call::<Option<bool>>("suspend", Some(params))?)
     }
@@ -166,7 +229,7 @@ impl<'a> AnkiConnectClient<'a> {
     // this appears to be out of compliance with the API spec. But if were to just let this error
     // out when Anki 2.1.x gives the null-null response, this would happen too often for this
     // method to be useful.
-    pub fn unsuspend(&self, cards: &Vec<i64>) -> Result<Option<bool>, Box<std::error::Error>> {
+    pub fn unsuspend(&self, cards: &Vec<i64>) -> Result<Option<bool>, Box<Error>> {
         let params = json!({ "cards": &cards });
         Ok(self.call::<Option<bool>>("unsuspend", Some(params))?)
     }
@@ -183,7 +246,7 @@ impl<'a> AnkiConnectClient<'a> {
     // ========== Media =====================================================
     // ======================================================================
 
-    pub fn delete_media_file(&self, filename: &str) -> Result<(), Box<std::error::Error>> {
+    pub fn delete_media_file(&self, filename: &str) -> Result<(), Box<Error>> {
         let params = json!({ "filename": &filename });
         Ok(self.call::<()>("deleteMediaFile", Some(params))?)
     }
